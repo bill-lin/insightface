806ef48e2df39f7cf22dd46db6bcb2c8cbfde9a2
bill lin <bill.lin@hellopupil.com>
2019-03-27 17:27:21 +0000

    fix for python 3

--- src/train_triplet.py
+++ src/train_triplet.py
@@ -174,7 +174,7 @@ def train_net(args):
     ctx = []
     cvd = os.environ['CUDA_VISIBLE_DEVICES'].strip()
     if len(cvd)>0:
-      for i in xrange(len(cvd.split(','))):
+      for i in range(len(cvd.split(','))):
         ctx.append(mx.gpu(i))
     if len(ctx)==0:
       ctx = [mx.cpu()]
